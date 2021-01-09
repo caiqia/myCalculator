1eaf3b4e4deec2ed80e3dd7003e1cc4d617c9bca
Qiaoshan Cai <qiaoshan.cai@mystream.fr>
2021-01-09 18:23:15 +0100

    qiaoshan commit:symfony calculator

---
+++ src/Controller/CalculatorController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CalculatorController extends AbstractController
+{
+    /**
+     * @Route("/calculate", name="calculator")
+     */
+    public function index(): Response
+    {
+        return $this->render('calculator/index.html.twig', [
+            'controller_name' => 'CalculatorController',
+        ]);
+    }
+    
+}
